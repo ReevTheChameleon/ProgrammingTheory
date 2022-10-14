fcd83904da27dd4e499188f8367b490cf37b4aec
ReevTheChameleon <reevthechameleon@gmail.com>
2022-10-14 19:07:02 +0700

    Revise Movement Code
    - Implement smooth turn logic
    - Adjust move logic

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 using Chameleon;
+using System.Collections;
+using System;
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
@@ -10,14 +12,19 @@ using UnityEditor;
 public class PlayerController : LoneMonoBehaviour<PlayerController>{
 	PlayerInput playerInput;
 	AnimationPlayer animPlayer;
+	Rigidbody rb;
 	
 	[Header("InputMove")]
 	[SerializeField] InputActionID actionIDMove;
 	[SerializeField] AnimationClip clipIdle;
 	[SerializeField] AnimationClip clipRun;
 	[SerializeField] float transitionTime = 0.2f;
-	private bool bMoving;
 	private Vector2 v2Direction;
+	private LoneCoroutine routineMove = new LoneCoroutine();
+	private const float RUNSPEED = 2.939501f/0.7f;
+	private bool bMoving;
+	private LoneCoroutine routineTurn = new LoneCoroutine();
+	private float targetFacingAngle;
 
 	[Header("inputLook")]
 	[SerializeField] InputActionID actionIDLook;
@@ -34,27 +41,42 @@ public class PlayerController : LoneMonoBehaviour<PlayerController>{
 	protected override void Awake(){
 		base.Awake();
 		playerInput = GetComponent<PlayerInput>();
-		animPlayer = GetComponentInChildren<AnimationPlayer>();
+		animPlayer = GetComponent<AnimationPlayer>();
+		rb = GetComponent<Rigidbody>();
 	}
 	void OnEnable(){
 		playerInput.actions[actionIDMove].performed += onInputMove;
 		playerInput.actions[actionIDMove].canceled += onInputIdle;
 		playerInput.actions[actionIDLook].performed += onInputLook;
 		playerInput.actions[actionIDZoom].performed += onInputZoom;
+		//routineTurn.evOnStop += unsubscribeRootMotionOverride;
 	}
 	void OnDisable(){
 		playerInput.actions[actionIDMove].performed -= onInputMove;
 		playerInput.actions[actionIDMove].canceled -= onInputIdle;
 		playerInput.actions[actionIDLook].performed -= onInputLook;
 		playerInput.actions[actionIDZoom].performed -= onInputZoom;
+		routineTurn.stop(this);
+		//routineTurn.evOnStop -= unsubscribeRootMotionOverride;
 	}
+	private Vector3 vMoveDirection;
+	[SerializeField] float turnSpeed;
+	private Action dRedirectRootMotion;
 	private void onInputMove(InputAction.CallbackContext context){
 		v2Direction = context.ReadValue<Vector2>();
-		faceDirection(v2Direction.polarAngle()*Mathf.Rad2Deg -90.0f);
+		targetFacingAngle = v2Direction.polarAngle()*Mathf.Rad2Deg -90.0f; //delta from tVCamTarget forward
+		routineTurn.start(this,rfTurn(targetFacingAngle));
+		routineMove.start(this,rfMove());
+		vMoveDirection = Quaternion.Euler(0.0f,-targetFacingAngle,0.0f)*tVCamTarget.forward;
+		//dRedirectRootMotion = ()=>{redirectRootMotion(vDirection);};
+		//animPlayer.evOnLateUpdate += dRedirectRootMotion;
+		//faceDirection(v2Direction.polarAngle()*Mathf.Rad2Deg -90.0f);
 		animPlayer.transitionTo(clipRun,transitionTime);
 		bMoving = true;
 	}
 	private void onInputIdle(InputAction.CallbackContext context){
+		routineTurn.stop(this);
+		routineMove.stop(this);
 		animPlayer.transitionTo(clipIdle,transitionTime);
 		bMoving = false;
 	}
@@ -68,13 +90,17 @@ public class PlayerController : LoneMonoBehaviour<PlayerController>{
 			v2LookRangeVertical.y
 		);
 		tVCamTarget.eulerAngles = eulerAngles;
-		if(bMoving)
-			faceDirection(v2Direction.polarAngle()*Mathf.Rad2Deg -90.0f);
+		if(bMoving){
+			//faceDirection(v2Direction.polarAngle()*Mathf.Rad2Deg -90.0f);
+			faceDirection(facingAngle);
+		}
 	}
+	private float facingAngle;
 	private void faceDirection(float angle){ //delta angle from tVCamTarget forward
 		/* Trick from Unity tutorial */
 		transform.setEulerY(tVCamTarget.eulerAngles.y-angle);
 		tVCamTarget.setLocalEulerY(angle);
+		vMoveDirection = transform.rotation*Vector3.forward;
 	}
 	private void onInputZoom(InputAction.CallbackContext context){
 		float delta = inputZoomSensitivity*context.ReadValue<float>(); //usually Windows is +/-120
@@ -85,6 +111,28 @@ public class PlayerController : LoneMonoBehaviour<PlayerController>{
 				v2ZoomRange.y
 			)
 		;
+	}
+	private IEnumerator rfTurn(float angleEnd){
+		float angleStart = -transform.eulerAngles.y+tVCamTarget.eulerAngles.y; //left-handed
+		float t = 0.0f;
+		float turnTime = Mathf.Abs(Mathf.DeltaAngle(angleStart,angleEnd))/turnSpeed;
+		WaitForFixedUpdate wait = new WaitForFixedUpdate();
+		while(t < 1.0f){
+			yield return null;
+			facingAngle = Mathf.LerpAngle(angleStart,angleEnd,t);
+			faceDirection(facingAngle);
+			t += Time.deltaTime/turnTime;
+		}
+		facingAngle = angleEnd;
+		faceDirection(angleEnd);
+	}
+	private IEnumerator rfMove(){
+		WaitForFixedUpdate wait = new WaitForFixedUpdate();
+		while(true){
+			rb.position += RUNSPEED*Time.fixedDeltaTime*
+				(Quaternion.Euler(0.0f,-targetFacingAngle,0.0f)*tVCamTarget.forward);
+			yield return wait;
+		}
 	}
 }
 
